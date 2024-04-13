1f05f70049b61dbce97b74228d44fa127a74c4f7
Marcus Reis <mvsantosreis@gmail.com>
2024-04-13 16:35:13 +0100

    CustomerFormType

---
+++ src/Form/CustomerFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class CustomerFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotNull([
+                        'message' => 'Email can not be blank',
+                    ]),
+                    new Email(),
+                ]
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'constraints' => [
+                    new NotNull(),
+                ]
+            ])
+            // ->add('cart')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}
